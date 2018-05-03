36dc5f7dd801c5be2d315cfe8e3cb06b46494c7d
Shawn Junell <Shawn.Junell@pemco.com>
2018-05-03 14:24:54 -0700

    update TestSJ

--- scripts/update_version.py
+++ scripts/update_version.py
@@ -5,6 +5,7 @@ FILELOC = sys.argv[1]
 ver = open(FILELOC+"/VERSION").readline()
 
 if ver is not None:
+    
     line = list(ver.rstrip())
     new_patch_num = int(line[len(line)-1])+1
     line[len(line)-1] = str(new_patch_num)
