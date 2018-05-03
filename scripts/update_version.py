175f07d7b1a6eeed22f7adc75b4c1f10760ac13c
Shawn Junell <Shawn.Junell@pemco.com>
2018-05-03 15:24:10 -0700

    update update_version.py

--- scripts/update_version.py
+++ scripts/update_version.py
@@ -9,10 +9,10 @@ if ver is not None:
     line = list(ver.rstrip())
     new_patch_num = int(line[len(line)-1])+1
     line[len(line)-1] = str(new_patch_num)
-    ver = ''.join(line)
+    new_ver = ''.join(line)
 
     file = open(FILELOC+"/VERSION", 'w')
-    file.write(ver)
+    file.write(new_ver)
     file.close()
 
 else:
