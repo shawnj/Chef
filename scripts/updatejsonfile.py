ba8812da7e764ecc63642a0c670cbac90f073cc9
Shawn Junell <Shawn.Junell@pemco.com>
2018-05-16 17:56:26 -0700

    update python script

---
+++ scripts/updatejsonfile.py
@@ -0,0 +1,17 @@
+import os
+import json
+import sys
+
+file = sys.argv[1]
+buildsite = sys.argv[2]
+buildnum = sys.argv[3]
+
+with open(file, "r") as jsonFile:
+    data = json.load(jsonFile)
+
+tmp = data["default_attributes"]["IPS_Sites"][buildsite.upper()]["BUILD"]
+
+data["default_attributes"]["IPS_Sites"][buildsite.upper()]["BUILD"] = buildnum
+
+with open(file, "w") as jsonFile:
+    json.dump(data, jsonFile)
